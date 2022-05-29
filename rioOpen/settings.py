8a0f4492db6ddaeb5e6a1aa40d2adcf8ff1cd731
LAPTOP-HCATFVOS\jua-d <juan.david.208080@gmail.com>
2022-05-29 09:44:52 -0500

    adding gunicorn, fixing hosts

--- rioOpen/settings.py
+++ rioOpen/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '05egi518h8uq@tv!f38*+0g!+$v*(0ne@4d2ptb%zc)95$szhq'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['rionegroopen.herokuapp.com']
 
 
 # Application definition
